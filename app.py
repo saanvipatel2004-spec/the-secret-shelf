6ed5a167999692d5bf416e8783f015881e56743a
Saanvi Patel <saanvipatel@mac.myfiosgateway.com>
2026-06-30 21:50:44 -0400

    Add mail timeout for Render

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_TIMEOUT"] = 10
 
 
 mail = Mail(app)
